42adec10d98ac8591b58b4cc25d43619cbf6e8d7
Jake Schoolmeesters <j.schoolmee@gmail.com>
2023-07-12 22:49:53 -0500

    moving navigation bar

--- components/navbar.js
+++ components/navbar.js
@@ -16,24 +16,21 @@ const MENU_LIST = [
     const [activeIdx, setActiveIdx] = useState(-1);
 
     return (
-        <header>
-            <nav className="bg-white border-gray-200 dark:bg-gray-900">
-                <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    {MENU_LIST.map((menu, idx) => (
-                    <li
-                        className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                        onClick={() => {
-                        setActiveIdx(idx);
-                        setNavActive(false);
-                        }}
-                        key={menu.text}
-                    >
-                        <NavItem active={activeIdx === idx} {...menu} />
-                    </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
+        <nav>
+            <ul>
+                {MENU_LIST.map((menu, idx) => (
+                <li
+                    onClick={() => {
+                    setActiveIdx(idx);
+                    setNavActive(false);
+                    }}
+                    key={menu.text}
+                >
+                    <NavItem active={activeIdx === idx} {...menu} />
+                </li>
+                ))}
+            </ul>
+        </nav>
     );
 
   }
